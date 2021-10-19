963079daf9baeb855cca1f9b510a6f455071eddf
Matthew Finerty <evilarceus@gmail.com>
2021-10-19 01:53:14 -0700

    GS: Fix padding in TRXREG register

--- src/GS/Registers/GSReg_TRXREG.cs
+++ src/GS/Registers/GSReg_TRXREG.cs
@@ -12,9 +12,9 @@ namespace BinarySerializer.PS2
             s.SerializeBitValues<ushort>(bitFunc =>
             {
                 RRW = bitFunc(RRW, 12, name: nameof(RRW));
-                bitFunc(default, 19, name: "Padding");
+                bitFunc(default, 20, name: "Padding");
                 RRH = bitFunc(RRH, 12, name: nameof(RRH));
-                bitFunc(default, 19, name: "Padding");
+                bitFunc(default, 20, name: "Padding");
             });
         }
     }
