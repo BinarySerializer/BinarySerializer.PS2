5553c0668e13bdc524f908236203096a3603110f
Matthew Finerty <evilarceus@gmail.com>
2021-10-19 00:59:01 -0700

    DMA: Add TagID enum for ID property

--- src/DMA/Chain_DMAtag.cs
+++ src/DMA/Chain_DMAtag.cs
@@ -4,7 +4,7 @@ namespace BinarySerializer.PS2
     {
         public ushort QWC { get; set; }
         public byte PCE { get; set; }
-        public byte ID { get; set; }
+        public TagID ID { get; set; }
         public byte IRQ { get; set; }
         public uint ADDR { get; set; }
         public byte SPR { get; set; }
@@ -16,11 +16,23 @@ namespace BinarySerializer.PS2
                 QWC = (ushort)bitFunc(QWC, 16, name: nameof(QWC));
                 bitFunc(default, 10, name: "Padding");
                 PCE = (byte)bitFunc(PCE, 2, name: nameof(PCE));
-                ID = (byte)bitFunc(ID, 3, name: nameof(ID));
+                ID = (TagID)bitFunc((int)ID, 3, name: nameof(ID));
                 IRQ = (byte)bitFunc(IRQ, 1, name: nameof(IRQ));
                 ADDR = (uint)bitFunc(ADDR, 31, name: nameof(ADDR));
                 SPR = (byte)bitFunc(SPR, 1, name: nameof(SPR));
             });
+        }
+
+        public enum TagID
+        {
+            REFE_CNTS, // refe for source chain tag, cnts for destination chain tag
+            CNT,
+            NEXT,
+            REF,
+            REFS,
+            CALL,
+            RET,
+            END
         }
     }
 }
