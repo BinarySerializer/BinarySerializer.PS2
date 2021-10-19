f80ecaae594aea8cc079215a3910374e5971fcd2
Matthew Finerty <evilarceus@gmail.com>
2021-10-19 01:52:49 -0700

    GS: Create PixelStorageFormat enum

--- src/GS/GS.cs
+++ src/GS/GS.cs
@@ -13,6 +13,25 @@ namespace BinarySerializer.PS2
             {
                 VRAM_Bytes = new byte[1024 * 1024 * 4]; // 4 MB
             }
+
+            // TODO: Add functions for writing and reading image data
+        }
+
+        public enum PixelStorageFormat
+        {
+            PSMCT32 = 0x00,
+            PSMCT24 = 0x01,
+            PSMCT16 = 0x02,
+            PSMCT16S = 0x0A,
+            PSMT8 = 0x13,
+            PSMT4 = 0x14,
+            PSMT8H = 0x1B,
+            PSMT4HL = 0x24,
+            PSMT4HH = 0x2C,
+            PSMZ32 = 0x30,
+            PSMZ24 = 0x31,
+            PSMZ16 = 0x32,
+            PSMZ16S = 0x3A
         }
     }
 }
--- src/GS/Registers/GSReg_BITBLTBUF.cs
+++ src/GS/Registers/GSReg_BITBLTBUF.cs
@@ -6,10 +6,10 @@ namespace BinarySerializer.PS2
 
         public int SBP { get; set; }
         public int SBW { get; set; }
-        public int SPSM { get; set; }
+        public GS.PixelStorageFormat SPSM { get; set; }
         public int DBP { get; set; }
         public int DBW { get; set; }
-        public int DPSM { get; set; }
+        public GS.PixelStorageFormat DPSM { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
@@ -19,13 +19,13 @@ namespace BinarySerializer.PS2
                 bitFunc(default, 2, name: "Padding");
                 SBW = bitFunc(SBW, 6, name: nameof(SBW));
                 bitFunc(default, 2, name: "Padding");
-                SPSM = bitFunc(SPSM, 6, name: nameof(SPSM));
+                SPSM = (GS.PixelStorageFormat)bitFunc((int)SPSM, 6, name: nameof(SPSM));
                 bitFunc(default, 2, name: "Padding");
                 DBP = bitFunc(DBP, 14, name: nameof(DBP));
                 bitFunc(default, 2, name: "Padding");
                 DBW = bitFunc(DBW, 6, name: nameof(DBW));
                 bitFunc(default, 2, name: "Padding");
-                DPSM = bitFunc(DPSM, 6, name: nameof(DPSM));
+                DPSM = (GS.PixelStorageFormat)bitFunc((int)DPSM, 6, name: nameof(DPSM));
                 bitFunc(default, 2, name: "Padding");
             });
         }
