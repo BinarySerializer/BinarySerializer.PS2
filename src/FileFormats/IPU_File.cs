60d4746616500cbe95afc50507f89ddcc3f93eda
Matthew Finerty <evilarceus@gmail.com>
2021-11-03 22:55:11 -0700

    IPU: Basic IPU file parsing

---
+++ src/FileFormats/IPU_File.cs
@@ -0,0 +1,21 @@
+namespace BinarySerializer.PS2
+{
+    public class IPU_File : BinarySerializable
+    {
+        public uint DataSize { get; set; }
+        public ushort Width { get; set; }
+        public ushort Height { get; set; }
+        public uint FrameCount { get; set; }
+        public byte[] FrameData { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            s.SerializeMagicString("ipum", 4);
+            DataSize = s.Serialize<uint>(DataSize, name: nameof(DataSize));
+            Width = s.Serialize<ushort>(Width, name: nameof(Width));
+            Height = s.Serialize<ushort>(Height, name: nameof(Height));
+            FrameCount = s.Serialize<uint>(FrameCount, name: nameof(FrameCount));
+            FrameData = s.SerializeArray<byte>(FrameData, DataSize - 8, name: nameof(FrameData));
+        }
+    }
+}
