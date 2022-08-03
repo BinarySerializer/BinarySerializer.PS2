4e05980676358c78ed587d4043507ca5162dd344
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-08-03 14:31:08 +0200

    Fix reading VIFcode flags

--- src/VPU/VIFcode_Unpack.cs
+++ src/VPU/VIFcode_Unpack.cs
@@ -9,17 +9,23 @@ namespace BinarySerializer.PS2
             VL = (UnpackVL)((int)vifcode.CMD & 0x03); // Bits 0-1
             SIZE = vifcode.NUM;
             ADDR = vifcode.IMMEDIATE & 0x3FF; // Bits 0-9
-            USN = ((vifcode.IMMEDIATE >> 13) & 0x01) == 1; // Bit 14
-            FLG = ((vifcode.IMMEDIATE >> 14) & 0x01) == 1; // Bit 15
+            USN = ((vifcode.IMMEDIATE >> 14) & 0x01) == 1; // Bit 14
+            FLG = ((vifcode.IMMEDIATE >> 15) & 0x01) == 1; // Bit 15
         }
 
         public bool M { get; set; }
         public UnpackVN VN { get; set; }
         public UnpackVL VL { get; set; }
         public uint SIZE { get; set; }
-        public bool FLG { get; set; }
-        public bool USN { get; set; }
         public uint ADDR { get; set; }
+
+        /// <summary>
+        /// False if it's sign-extended. For example if a 16-bit value is unpacked then the remaining
+        /// 16 bits get set so the full 32-bit value remains signed.
+        /// </summary>
+        public bool USN { get; set; }
+
+        public bool FLG { get; set; }
 
         public override string ToString() => $"{VN}_{VL}, SIZE: {SIZE}, ADDR: {ADDR}, M: {M}, USN: {USN}, FLG: {FLG}";
 
